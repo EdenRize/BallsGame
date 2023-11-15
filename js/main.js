2ea7c73c5806c86234420e0f68fb4a7a28ca09ec
EdenRize <edenrize@gmail.com>
2023-11-15 17:43:43 +0200

    Add maxDiameter parameter to onBallClick()

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
   var randomInc = getRandomInt(20, 60)
   var newSize =
-    +elBall.innerText + randomInc >= 400 ? 100 : +elBall.innerText + randomInc
+    +elBall.innerText + randomInc >= maxDiameter
+      ? 100
+      : +elBall.innerText + randomInc
 
   elBall.style.width = newSize + 'px'
   elBall.style.height = newSize + 'px'
